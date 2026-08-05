f877a71f455f76dcf3ae428849a4e2feeab0e865
ujwaljain506-hash <ujwaljain506@gmail.com>
2026-08-06 00:52:46 +0530

    day23: leetcode 739 daily temperatures - monotonic stack

---
+++ day23_leetcode_739_daily_temperatures.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        answer = [0]*len(temperatures)
+        
+        stack = []
+        
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                    prev = stack.pop()
+                    answer[prev] = i - prev
+            stack.append(i)
+                
+                
+            
+        return answer
